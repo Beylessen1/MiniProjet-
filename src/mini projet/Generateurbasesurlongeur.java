3586c07a69f5f586484bca5e357c37519ebe30a8
Beylessen1 <beylessen.jendoubi@etudiant-enit.utm.tn>
2025-05-07 02:10:36 +0300

    Update Generateurbasesurlongeur.java

--- src/mini projet/Generateurbasesurlongeur.java
+++ src/mini projet/Generateurbasesurlongeur.java
@@ -1,4 +1,17 @@
 package miniprojet;
-
-//public class Generateurbasesurlongeur { }
+import java.util.ArrayList;
+public class Generateurbasesurlongeur implements GenerateurDeCandidats {
+  public List<Nom> genererCandidats(Nom nom, List<Nom> liste1){
+    int longueur= (nom.getNom()).length();
+    List<Nom> liste_candidats = new ArrayList<>();
+    for (Nom i : liste1){
+      int j = i.length();
+      if (j==longueur){
+        liste_candidats.add(i);
+      }
+     return liste_candidats;
+  }
+    
+  
+}
 //=- 3 lettre
