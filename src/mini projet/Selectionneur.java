b5c080fbf8dac4ad73a90341d6855276a47a05cd
Beylessen1 <beylessen.jendoubi@etudiant-enit.utm.tn>
2025-05-07 03:17:11 +0300

    Update Selectionneur.java

--- src/mini projet/Selectionneur.java
+++ src/mini projet/Selectionneur.java
@@ -3,5 +3,5 @@ package miniprojet;
 import java.util.List;
 
 public interface Selectionneur {
-     public List <Nom> est_acceptable (List<CoupleDeNomAvecScore>) ;
+     public List <Nom> est_acceptable (List<CoupleDeNomAvecScore> liste_noms_selectionnes) ;
 }
