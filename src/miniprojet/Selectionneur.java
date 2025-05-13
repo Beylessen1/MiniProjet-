c00b06f219ede751923dabe9c601a74206b8c0ee
Nebras-T <nebras.trabelsi@etudiant-enit.utm.tn>
2025-05-13 17:18:55 +0100

    Update Selectionneur.java

--- src/miniprojet/Selectionneur.java
+++ src/miniprojet/Selectionneur.java
@@ -3,5 +3,5 @@ package miniprojet;
 import java.util.List;
 
 public interface Selectionneur {
-     public List <Nom> est_acceptable (List<CoupleDeNomAvecScore> liste_noms_nonselectionnes) ;
+     public List <CoupleDeNomAvecScore> est_acceptable (List<CoupleDeNomAvecScore> liste_noms_nonselectionnes) ;
 }
